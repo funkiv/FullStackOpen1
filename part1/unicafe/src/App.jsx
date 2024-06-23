3a3c3eb8e6b5c2f9de62185c3775a8b84078b194
Charles Funk <funkch@dukes.jmu.edu>
2024-06-22 19:52:32 -0500

    Complete 1.7 step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,6 +13,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = good / total * 100 
 
   return (
     <div>
@@ -24,6 +27,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total} </p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }

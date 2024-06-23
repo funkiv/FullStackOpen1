dd738fd2f07120e89f4fab82a10a81be98c92c49
Charles Funk <funkch@dukes.jmu.edu>
2024-06-22 20:11:41 -0500

    Complete 1.9 unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,18 +13,21 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / total
   const positive = good / total * 100 
 
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total} </p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
-    </div>
-  )
-
+  if (total === 0) {
+    return <p>no feedback given</p>
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total} </p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
+      </div>
+    )
+    }
 }
 
 const App = () => {

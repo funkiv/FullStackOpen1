ccb7b795c5bb85ce5f7598c58c2d89fdbd169d1b
Charles Funk <funkch@dukes.jmu.edu>
2024-06-25 15:20:17 -0500

    Complete actual 2.4 step 9

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -36,35 +36,65 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ],
-    totalExercises: 0
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ],
+      totalExercises: 0
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ],
+      totalExercises: 0
+    }
+  ]
 
+  courses.forEach((course) => {
   course.totalExercises = course.parts.reduce((accumulator, part) => {
-    accumulator += part.exercises
-    return accumulator
-  }, 0)
+      accumulator += part.exercises
+      return accumulator
+    }, 0)
+  })
 
-  return <Course course={course} />
+  return (
+    <div>
+      {courses.map((course) => <Course key={course.id} course={course} />)}
+    </div>  
+  )
 }
 
 export default App

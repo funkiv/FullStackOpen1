992ff9a83ab70ad04fff66238058bab8cf34b092
Charles Funk <funkch@dukes.jmu.edu>
2024-06-27 15:17:27 -0500

    Complete 2.8 step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
+  //Event handlers for form
   const handleNameChange = (event) => {
     setNewName(event.target.value)  
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)  
   }
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if (persons.find(e => e.name == personObject.name)){
@@ -28,9 +34,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
